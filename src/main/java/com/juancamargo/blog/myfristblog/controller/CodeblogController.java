cd5c1093f207b985ea1290d50b831d573cc76d2d
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-04 18:15:46 -0300

    findby id criado  page pra detais tmb

--- src/main/java/com/juancamargo/blog/myfristblog/controller/CodeblogController.java
+++ src/main/java/com/juancamargo/blog/myfristblog/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import com.juancamargo.blog.myfristblog.model.Post;
 import com.juancamargo.blog.myfristblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,11 +19,21 @@ public class CodeblogController {
     @Autowired
     CodeblogService codeblogService;
 
-    @RequestMapping(value = "posts", method = RequestMethod.GET)
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public ModelAndView getPost(){
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts",posts);
         return mv;
     }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id")long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post",post);
+        return mv;
+    }
+
+
 }
