cae5a991af6c673dc9f47170a73dc881d6a0cb00
Juan Camargo <juan.09.camargo@gmail.com>
2021-09-04 11:36:16 -0300

    frist commit

---
+++ src/main/java/com/juancamargo/blog/myfristblog/MyfristblogApplication.java
@@ -0,0 +1,13 @@
+package com.juancamargo.blog.myfristblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfristblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfristblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juancamargo/blog/myfristblog/MyfristblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancamargo.blog.myfristblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfristblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
